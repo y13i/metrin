d782457f628209f350c59607ab03450770ea733e
Yoriki Yamaguchi <yamaguchi@cloudpack.jp>
2017-01-11 19:07:20 +0900

    bump up version

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 
 	app.Name = "metrin"
 	app.Usage = "Very simple CloudWatch CLI for Zabbix/Nagios/Sensu/Mackerel/etc."
-	app.Version = "0.0.4"
+	app.Version = "0.0.5"
 	app.EnableBashCompletion = true
 
 	app.Flags = []cli.Flag{
